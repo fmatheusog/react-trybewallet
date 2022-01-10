b4176f9e77d82b002541e3b9a050860047d47c3f
Felipe Matheus <fmatheusog@gmail.com>
2022-01-10 16:21:31 -0300

    Created routes using react-router-dom

--- src/App.js
+++ src/App.js
@@ -1,7 +1,13 @@
 import React from 'react';
+import { BrowserRouter } from 'react-router-dom';
+import Routes from './components/Routes';
 
 function App() {
-  return <div>Hello, TrybeWallet!</div>;
+  return (
+    <BrowserRouter>
+      <Routes />
+    </BrowserRouter>
+  );
 }
 
 export default App;
---
+++ src/components/Routes/index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { Switch, Route } from 'react-router-dom';
+
+import Login from '../../pages/Login';
+import Wallet from '../../pages/Wallet';
+
+const Routes = () => (
+  <Switch>
+    <Route exact path="/" component={ Login } />
+    <Route exact path="/wallet" component={ Wallet } />
+  </Switch>
+);
+
+export default Routes;
