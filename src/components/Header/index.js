8fe918adcf11111149c9cb4851322e159664e067
Felipe Matheus <fmatheusog@gmail.com>
2022-01-11 16:15:57 -0300

    Created Wallet page component inputs and selects

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,13 +1,24 @@
 import React from 'react';
 import { useSelector } from 'react-redux';
+import './style.css';
 
 const Header = () => {
-  const email = useSelector((state) => state);
+  const email = useSelector((state) => state.user.email);
 
   return (
-    <div>
-      <span>Trybe</span>
-      <span data-testid="email-field">{ email }</span>
+    <div className="header">
+      <div className="header-main">
+        <span className="header-trybe">Trybewallet</span>
+        <span data-testid="email-field">
+          Olá,
+          {' '}
+          { email }
+        </span>
+      </div>
+      <div className="header-currency">
+        <span data-testid="total-field">0</span>
+        <span data-testid="header-currency-field">BRL</span>
+      </div>
     </div>
   );
 };
--- src/pages/Wallet/index.js
+++ src/pages/Wallet/index.js
@@ -1,9 +1,63 @@
 import React from 'react';
+import Header from '../../components/Header';
 
 const Wallet = () => (
-  <div>
-    Wallet component
-  </div>
+  <>
+    <Header />
+    <div className="wallet-page">
+      <div className="expenses-add-form">
+        <label htmlFor="value-input">
+          Valor da despesa:
+          <input
+            type=""
+            data-testid="value-input"
+            id="value-input"
+          />
+        </label>
+
+        <label htmlFor="description-input">
+          Descrição:
+          <input
+            type="text"
+            data-testid="description-input"
+            id="description-input"
+          />
+        </label>
+
+        Moeda:
+        <select
+          data-testid="currency-input"
+          id="currency-input"
+        >
+          <option>Teste</option>
+        </select>
+
+        Método de pagamento:
+        <select
+          data-testid="method-input"
+          id="method-input"
+        >
+          <option value="Dinheiro">Dinheiro</option>
+          <option value="Dinheiro">Cartão de débito</option>
+          <option value="Dinheiro">Cartão de crédito</option>
+        </select>
+
+        Categoria:
+        <select
+          data-testid="tag-input"
+          id="tag-input"
+        >
+          <option>Teste</option>
+        </select>
+
+        <button
+          type="button"
+        >
+          Adicionar despesa
+        </button>
+      </div>
+    </div>
+  </>
 );
 
 export default Wallet;
