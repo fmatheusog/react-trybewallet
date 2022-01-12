81cc4b5ce40e36aeb324a7850be09b2141656d4d
Felipe Matheus <fmatheusog@gmail.com>
2022-01-12 18:12:37 -0300

    Removed BrowserRouter from App.js, it was already in index.js

--- src/App.js
+++ src/App.js
@@ -1,12 +1,9 @@
 import React from 'react';
-import { BrowserRouter } from 'react-router-dom';
 import Routes from './components/Routes';
 
 function App() {
   return (
-    <BrowserRouter>
-      <Routes />
-    </BrowserRouter>
+    <Routes />
   );
 }
 
