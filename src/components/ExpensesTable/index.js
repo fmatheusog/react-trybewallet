397d84e6c9ac1f664513a6e307a133c03ab4853e
Felipe Matheus <fmatheusog@gmail.com>
2022-01-13 02:46:37 -0300

    Expenses table connected to redux store global state

--- src/components/ExpensesTable/index.js
+++ src/components/ExpensesTable/index.js
@@ -1,6 +1,9 @@
 import React from 'react';
+import { useSelector } from 'react-redux';
 
 function ExpensesTable() {
+  const expenses = useSelector((state) => state.wallet.expenses);
+
   return (
     <div>
       <table className="expenses-table">
@@ -13,7 +16,28 @@ function ExpensesTable() {
           <th>Câmbio utilizado</th>
           <th>Valor convertido</th>
           <th>Moeda de conversão</th>
+          <th>Editar/Excluir</th>
         </tr>
+        {
+          expenses.map((expense) => (
+            <tr key={ expense.id }>
+              <td>{ expense.description }</td>
+              <td>{ expense.tag }</td>
+              <td>{ expense.method }</td>
+              <td>{ expense.value }</td>
+              <td>{ expense.exchangeRates[expense.currency].name }</td>
+              <td>{ Number(expense.exchangeRates[expense.currency].ask).toFixed(2) }</td>
+              <td>
+                {
+                  Number(expense.value * expense.exchangeRates[expense.currency].ask)
+                    .toFixed(2)
+                }
+              </td>
+              <td>Real</td>
+              <td>{ }</td>
+            </tr>
+          ))
+        }
       </table>
     </div>
   );
